48ae8913a75e3b084275459f3cf72445f3ab8aed
nwebsdale <natashawebsdale@hotmail.co.uk>
2021-01-13 16:15:23 +0000

    Icons and background change with weather

--- apps.js
+++ apps.js
@@ -68,7 +68,6 @@ function showPositionWeather(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(displayWeather);
 }
-
 // Display Current Weather
 function displayWeather(response) {
   console.log(response.data);
@@ -95,6 +94,84 @@ function displayWeather(response) {
   let cityName = response.data.name;
   let country = response.data.sys.country;
   document.querySelector("#location").innerHTML = `${cityName}, ${country}`;
+
+  // Update main icon and background image
+
+  let mainIcon = document.querySelector("#main-icon");
+  let background = document.querySelector("#background");
+
+  // Cloud
+
+  if (response.data.weather[0].icon === "02d" || "02n" || "03d" || "03n") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-cloud-sun", "main-icon-cloud");
+  }
+
+  if (response.data.weather[0].icon === "04d" || "04n") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-cloud", "main-icon-cloud");
+  }
+
+  // Sun
+
+  if (response.data.weather[0].icon === "01d") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-sun", "main-icon-dry");
+    background.setAttribute("class", "");
+    background.classList.add("background-clear-day");
+  }
+
+  if (response.data.weather[0].icon === "01n") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-moon", "main-icon-cloud");
+    background.setAttribute("class", "");
+    background.classList.add("background-clear-night");
+  }
+
+  // Thunderstorm
+
+  if (response.data.weather[0].icon === "11d") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-bolt", "main-icon-wet");
+    background.setAttribute("class", "");
+    background.classList.add("background-bolt");
+  }
+
+  // Drizzle
+
+  if (response.data.weather[0].icon === "09d") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-cloud-rain", "main-icon-wet");
+    background.setAttribute("class", "");
+    background.classList.add("background-drizzle");
+  }
+
+  // Rain
+
+  if (response.data.weather[0].icon === "10d") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-cloud-showers-heavy", "main-icon-wet");
+    background.setAttribute("class", "");
+    background.classList.add("background-rain");
+  }
+
+  // Snow
+
+  if (response.data.weather[0].icon === "13n") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-snowflake", "main-icon-cloud");
+    background.setAttribute("class", "");
+    background.classList.add("background-snow");
+  }
+
+  // Fog
+
+  if (response.data.weather[0].icon === "50d") {
+    mainIcon.setAttribute("class", "");
+    mainIcon.classList.add("fas", "fa-smog", "main-icon-cloud");
+    background.setAttribute("class", "");
+    background.classList.add("background-fog");
+  }
 }
 
 // Display Current Date/Time
